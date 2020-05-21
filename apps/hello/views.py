61fe2d2da836ccafe1c28c3a93c1c7fb1dac0f8b
ruszhov <ruszhov@ukr.net>
2020-05-21 13:19:38 +0300

    ticket:1 error fixing

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -5,6 +5,6 @@ import datetime
 
 
 def hello(request):
-    contact = Contact.objects.get(pk=1)
+    contact = Contact.objects.first()
     age = int((datetime.date.today() - contact.date_of_birth).days / 365.25)
     return render(request, 'hello/index.html', locals())
