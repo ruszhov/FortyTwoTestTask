6933da708b99325706d2c61d7d233328be9fc464
ruszhov <ruszhov@ukr.net>
2020-05-22 12:38:11 +0300

    ticket:1 migrations adding

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -8,8 +8,6 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Deleting model 'Contact'
-        db.delete_table(u'hello_contact')
         # Adding model 'Contact'
         db.create_table(u'hello_contact', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
@@ -19,6 +17,8 @@ class Migration(SchemaMigration):
             ('bio', self.gf('django.db.models.fields.TextField')(max_length=200, null=True, blank=True)),
             ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
             ('skype', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('jabber', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('other_contacts', self.gf('django.db.models.fields.TextField')(max_length=200, blank=True)),
         ))
         db.send_create_signal(u'hello', ['Contact'])
 
@@ -34,7 +34,9 @@ class Migration(SchemaMigration):
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {'max_length': '200', 'blank': 'True'}),
             'skype': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
         }
     }
