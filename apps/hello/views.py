8783b9c7ae26975342797ba57ab93d35f86d0271
ruszhov <ruszhov@ukr.net>
2020-05-22 12:46:07 +0300

    ticket:1 migrations adding

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -5,6 +5,6 @@ import datetime
 
 
 def hello(request):
-    contact = Contact.objects.first()
+    contact = Contact.objects.get(pk=1)
     age = int((datetime.date.today() - contact.date_of_birth).days / 365.25)
     return render(request, 'hello/index.html', locals())
