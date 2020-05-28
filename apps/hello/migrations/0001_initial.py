81c84b66054d010f6606bd837a96b288f2f6a962
ruszhov <ruszhov@ukr.net>
2020-05-28 16:34:56 +0300

    ticket:5 test pass

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -6,6 +6,9 @@ from south.v2 import SchemaMigration
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Deleting model 'Contact'
+        db.delete_table(u'hello_contact')
+
         # Adding model 'Contact'
         db.create_table(u'hello_contact', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
