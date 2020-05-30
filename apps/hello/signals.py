13378c1c46069d7bdf6a65140fd643e3e01c74c2
ruszhov <ruszhov@ukr.net>
2020-05-30 16:56:05 +0300

    ticket:8 test pass

--- apps/hello/signals.py
+++ apps/hello/signals.py
@@ -1,3 +1,4 @@
+# pylint: disable=C0321
 from .models import ModelActionLog
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
@@ -8,6 +9,9 @@ ignored_models = ['ModelActionLog', 'LogEntry', 'ContentType']
 
 @receiver(post_save, dispatch_uid='nope')
 def post_save_signal(sender, instance, created, dispatch_uid='nope', **kwargs):
+    '''
+    post_save signal implementation for action 'create', 'update'
+    '''
     if created and sender.__name__ not in ignored_models:
         ModelActionLog.objects.create(
             model_name=sender.__name__, instance=instance, action='create')
@@ -18,6 +22,9 @@ def post_save_signal(sender, instance, created, dispatch_uid='nope', **kwargs):
 
 @receiver(post_delete, dispatch_uid='nope')
 def post_delete_signal(sender, instance, dispatch_uid='nope', **kwargs):
+    '''
+    post_delete signal implementation for 'delete' actions
+    '''
     if sender.__name__ not in ignored_models:
         ModelActionLog.objects.create(
             model_name=sender.__name__, instance=instance, action='delete')
